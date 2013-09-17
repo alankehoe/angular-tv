07a23b61732052d2db949043641cc621cb4965fe
Alan Kehoe <alankehoe111@hotmail.com>
2013-09-17 11:29:22 +0100

    added controller code

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,8 +1,15 @@
-app.controller("mainController", function($scope, $http){
+app.controller("mainController", function ($scope, $http) {
 
-    $scope.init = function() {
+    $scope.apiKey = "6f950ea6ed5ae98f5af25c6925488b5c";
+    $scope.init = function () {
+        //API requires a start date
+        var today = new Date();
+        //Create the date string and ensure leading zeros if required
+        var apiDate = today.getFullYear() + ("0" + (today.getMonth() + 1)).slice(-2) + "" + ("0" + today.getDate()).slice(-2);
+        $http.jsonp('http://api.trakt.tv/calendar/premieres.json/' + $scope.apiKey + '/' + apiDate + '/' + 30 + '/?callback=JSON_CALLBACK').success(function (data) {
+            console.log(data);
+        }).error(function (error) {
 
+            });
     };
-
 });
-
