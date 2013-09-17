d2062383d343c386b3e0d6df190b41b7b1a79917
Alan Kehoe <alankehoe111@hotmail.com>
2013-09-17 13:21:43 +0100

    Move out the genre filter to its own js file

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -43,20 +43,3 @@ app.controller("mainController", function ($scope, $http) {
     };
 });
 
-app.filter('isGenre', function() {
-    return function(input, genre) {
-        if (typeof genre == 'undefined' || genre == null) {
-            return input;
-        } else {
-            var out = [];
-            for (var a = 0; a < input.length; a++) {
-                for (var b = 0; b < input[a].show.genres.length; b++) {
-                    if (input[a].show.genres[b] == genre) {
-                        out.push(input[a]);
-                    }
-                }
-            }
-            return out;
-        }
-    }
-})
